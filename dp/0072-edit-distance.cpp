aac803aa50b8998298b963b4ca6581f4f9d80bf4
zhaoxuyan <526161421@qq.com>
2020-11-11 16:00:43 +0800

    2020.11.11

---
+++ dp/0072-edit-distance.cpp
@@ -0,0 +1,65 @@
+/*
+ * 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数
+ *
+ * 你可以对一个单词进行如下三种操作：
+ *
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ *
+ * 输入：word1 = "horse", word2 = "ros"
+ * 输出：3
+ * 解释：
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ *
+ * 输入：word1 = "intention", word2 = "execution"
+ * 输出：5
+ * 解释：
+ * intention -> inention (删除 't')
+ * inention -> enention (将 'i' 替换为 'e')
+ * enention -> exention (将 'n' 替换为 'x')
+ * exention -> exection (将 'n' 替换为 'c')
+ * exection -> execution (插入 'u')
+ *
+ *
+ * 解析：
+ * 作者：ikaruga
+ * 链接：https://leetcode-cn.com/problems/edit-distance/solution/edit-distance-by-ikaruga/
+
+ */
+
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        int dp[m + 1][n + 1];
+        memset(dp, 0, sizeof(dp));
+        // base case 如果word2为空
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        // base case 如果word1为空
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
--- dp/0509-Fibonacci-Number.cpp
+++ dp/0509-Fibonacci-Number.cpp
@@ -17,7 +17,7 @@ public:
         std::vector<int> dp(N + 1, 0);
         // base case
         dp[1] = dp[2] = 1;
-        for (int i = 1; i <= N; ++i) {
+        for (int i = 3; i <= N; ++i) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[N];
