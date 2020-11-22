fd2f726e4ba07dbd85383474d17643233f7d54c6
zhaoxuyan <zhaoxuyan@me.com>
2020-11-22 21:24:16 +0800

    2020.11.22

---
+++ Array/Recursion/0230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,64 @@
+/*
+ * 230. 二叉搜索树中第K小的元素
+ * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+ *
+ * 输入: root = [3,1,4,null,2], k = 1
+       3
+      / \
+     1   4
+      \
+       2
+    输出: 1
+
+
+    输入: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+    输出: 3
+
+ */
+#include <algorithm>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int kthSmallest(TreeNode *root, int k) {
+        traverse(root, k);
+        return res;
+    }
+    void traverse(TreeNode *root, int k) {
+        if (!root) {
+            return;
+        }
+        traverse(root->left, k);
+        // 二叉搜索书的中序遍历是升序
+        rank++;
+        if (k == rank) {
+            res = root->val;
+            return;
+        }
+        traverse(root->right, k);
+    }
+
+private:
+    int res = 0;
+    int rank = 0;
+};
